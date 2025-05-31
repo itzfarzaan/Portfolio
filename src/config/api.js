218224d9b94d865b8e311ad85095c98c2ae76ab5
itzfarzaan <farzaanali2004@gmail.com>
2025-05-31 13:39:04 +0530

    Updated API_URL to match Vercel's domain

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,5 @@
-// API configuration
-export const API_URL = 'https://portfolioserver-5m7j.onrender.com';
+// API configuration (From Render)
+// export const API_URL = 'https://portfolioserver-5m7j.onrender.com';
+
+// API configuration (From Vercek)
+export const API_URL = 'https://portfolio-server-tau-eosin.vercel.app';
